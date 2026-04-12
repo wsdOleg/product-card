acaedcce3ffe0835be11c9612a25353eae3c0654
Oleg <olov_1990@bk.ru>
2026-04-12 20:29:37 +0500

    fix: tabulation

--- homework-6.js
+++ homework-6.js
@@ -13,11 +13,11 @@ const user = {
 
 //4 данные автомобиля 
 const car = {
-brand: "Honda",
-model: "Civic 8 4d", 
-year: 2007, 
-color: "темно-синий",
-transmission: "АКПП"
+	brand: "Honda",
+	model: "Civic 8 4d", 
+	year: 2007, 
+	color: "темно-синий",
+	transmission: "АКПП"
 };
 
 car.owner = user;
