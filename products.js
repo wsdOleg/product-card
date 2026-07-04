e0586be848dc8f0489bc9c540081b0c582ad6df8
Oleg <olov_1990@bk.ru>
2026-07-04 17:20:14 +0500

    feat: add homework 8 product cards

---
+++ products.js
@@ -0,0 +1,73 @@
+export const products = [
+	{
+		id: 1,
+		name: "Гель для умывания ",
+		skinType: "нормальная",
+		description: "Глубоко увлажняют кожу лица, оставляя её мягкой и гладкой.",
+		ingredients: [
+			"пантенол",
+			"аллантоин",
+			"экстракт мальвы"
+		],
+		price: 699,
+		currency: "RUB",
+		imageUrl: "img/moisturizing_mousse.png"
+	},
+	{
+		id: 2,
+		name: "Маска для лица ",
+		skinType: "нормальная",
+		description: "Способствует удерживанию влаги в верхних слоях кожи.",
+		ingredients: [
+			"гиалуроновая кислота",
+			"натуральный увлажняющий фактор",
+			"пантенол"
+		],
+		price: 495,
+		currency: "RUB",
+		imageUrl: "img/moisturizing_mask.png"
+	},
+	{
+		id: 3,
+		name: "Гель для умывания ",
+		skinType: "нормальная",
+		description: "Интенсивно очищает, не повреждает защитный барьер кожи.",
+		ingredients: [
+			"березовый деготь",
+			"коллоидная сера",
+			"экстракты березы, ромашки, череды и чистотела"
+		],
+		price: 391,
+		currency: "RUB",
+		imageUrl: "img/cleansing_gel.png"
+	},
+	{
+		id: 4,
+		name: "Увлажняющий флюид",
+		skinType: "нормальная",
+		description: "Набор, состоящий из увлажняющего крема и маски.",
+		ingredients: [
+			"масло ши",
+			"лактулоза и комплекс аминокислот",
+			"календула, аллантоин, шалфей и молочная кислота"
+		],
+		price: 1510,
+		currency: "RUB",
+		imageUrl: "img/gift_set_1.png"
+	},
+	{
+		id: 5,
+		name: "Пенка для умывания ",
+		skinType: "нормальная",
+		description:
+			"Весь набор средств Invisible symphony: крем, маска, мусс и гель для умывания.",
+		ingredients: [
+			"муцин улитки",
+			"экстракт зелёного чая",
+			"экстракт солодки и пантенол"
+		],
+		price: 708,
+		currency: "RUB",
+		imageUrl: "img/gift_set_5.png"
+	}
+];
